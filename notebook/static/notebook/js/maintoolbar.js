cbfb4adb683c493c9a59099afe3428ae4691cbef
vincenttgao <vincenttgao@tencent.com>
2021-10-13 16:15:56 +0800

    fix: 去除文档链接，去除上传文件按钮

--- notebook/static/notebook/js/maintoolbar.js
+++ notebook/static/notebook/js/maintoolbar.js
@@ -37,7 +37,7 @@ define([
         // 'jupyter-notebook:show-all-run-time',
         // 'jupyter-notebook:hide-all-run-time',
         'jupyter-notebook:version-management',
-        'jupyter-notebook:upload-file',
+        // 'jupyter-notebook:upload-file',
         'jupyter-notebook:notebook-report'
     ];
 
